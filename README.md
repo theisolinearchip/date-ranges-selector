663b11d6bbe8d4254d4921e634a1e28259758814
koopaconan <koopaconan@gmail.com>
2018-04-05 09:29:29 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,103 @@
+## date-ranges-selector
+
+A jQuery plugin to create and manage date ranges.
+Written by [Albert Gonzalez](http://albertgonzalez.coffee) and released under [the Unlicense](http://unlicense.org/).
+
+[See it in action!](http://albertgonzalez.coffee/projects/multi-list/)
+
+### Install
+
+Download the *.js* and *.css* files or install them using **npm**:
+
+```markdown
+npm install date-ranges-selector
+```
+
+### Starting
+
+```markdown
+$(element).dateRangesSelector({options});
+```
+
+Available options:
+
+* new_date_range_text : Default text for the starting button
+* main_class_prefix : Css class for the plugin main container
+* date_format : Something like "D, dd/mm/yy". Used by the datepickers
+* selector : If true (enabled by default), adds an aditional selector that will be attached to every range)
+* selector_name : "appear" by default. The "name" of the selector (if provided
+* selector_options : Array of options to be shown in the selector. Each element is another array "text"-"value". Default: [ ["Display", "1"], ["Don't display", "0"] ]
+* use_timezone_offset : If true (enabled by default) uses timezone offsets when fetching dates
+
+Now you're ready!
+
+### Methods
+
+Add a new range:
+
+```markdown
+$(element).datesRangesSelector("addDateRange", {options});
+```
+
+Available options:
+
+* date_begin : Optional date_begin value in unixtime to set the first date of the new added element.
+* date_end : Optional date_end value in unixtime to set the second date of the new added element.
+* selector : Optional selector value (if selector is enabled)
+
+Remove an existing range: 
+
+```markdown
+$(element).datesRangesSelector("removeDateRange", [position (begins with 1)]);
+```
+
+Remove ALL ranges:
+
+```markdown
+Remove ALL ranges: $(element).datesRangesSelector("removeAllDateRanges");
+```
+
+Get all non-empty ranges:
+
+```markdown
+$(element).datesRangesSelector("getDateRanges");
+```
+
+Visually disable the plugin (the GET method won't return anything, but still can add elements via methods):
+
+```markdown
+$(element).datesRangesSelector("disable");
+```
+
+Visually enable the plugin:
+
+```markdown
+$(element).datesRangesSelector("enable");
+```
+
+### Events
+
+The events are triggered when adding or removing elements:
+
+```markdown
+$(element).on('datesRangesSelector.rangeAdded', function(event, date_begin, date_end, selector) {
+  console.log('Added range with values ' + date_begin + ', ' + date_end + ', ' + selector + '. Those values can be undefined if the range is added without default values');
+});
+
+$(element).on('datesRangesSelector.rangeRemoved', function(event, position) {
+  console.log('Range removed at position ' + position);
+});
+
+$(element).on('datesRangesSelector.allRangesRemoved', function(event, position) {
+  console.log('ALL ranges removed');
+});
+
+$(element).on('datesRangesSelector.becameFull', function(event, date_begin, date_end, selector) {
+  console.log('I was empty, but after that, I have at least one new element');
+});
+
+$(element).on('datesRangesSelector.becameEmpty', function(event) {
+  console.log('I wasn't empty (with at least one element) but now I amb after that last operation');
+});
+
+```
---
+++ date-ranges-selector.js
@@ -0,0 +1,312 @@
+/**
+* date-ranges-selector
+*
+* Author: Albert Gonzalez - http://www.albertgonzalez.coffee
+* 
+* Under the UNLICENSE (http://unlicense.org/)
+*
+* Methods:
+* Init: $(element).datesRangesSelector({options});
+*	Options:
+*		new_date_range_text : "+ Add a new date range",
+*		main_class_prefix : "drs",
+*		date_format : "D, dd/mm/yy",
+*		selector : true, (an aditional selector that will be attached to every range)
+*		selector_name : "appear",
+*		selector_options : [ ["Display", "1"], ["Don't display", "0"] ],
+*		use_timezone_offset : true
+* Remove: $(element).datesRangesSelector("remove");
+*
+* Add a new range: $(element).datesRangesSelector("addDateRange", {options});
+* 	Options:
+*		date_begin,
+*		date_end, (in unixtime, SECONDS. If set, will init the datepickers with those dates)
+*		selector (the same, but with the value from the selector)
+*
+* Remove an existing range: $(element).datesRangesSelector("removeDateRange", [position (begins with 1)]);
+* Remove ALL ranges: $(element).datesRangesSelector("removeAllDateRanges");
+*
+* Get all populated ranges: $(element).datesRangesSelector("getDateRanges");
+*
+* Visually disable the plugin (the GET method won't return anything disabled, but still can add elements via methods): $(element).datesRangesSelector("disable");
+* Visually enable the plugin: $(element).datesRangesSelector("enable");
+*
+* Events:
+* "datesRangesSelector.rangeAdded": Triggered when adding a range. Elements passed: position for the new range, date_begin, date_end, selector value (last three params can be undefined if empty)
+* "datesRangesSelector.rangeRemoved": Triggered when removing a range. Elements passed: position for the deleted range.
+* "datesRangesSelector.allRangesRemoved": Triggered when removing ALL ranges. No elements passed.
+* "datesRangesSelector.becameFull": Triggered when changing from no ranges to, at least, one. Elements passed: position for the new range, date_begin, date_end, selector value (last three params can be undefined if empty)
+* "datesRangesSelector.becameEmpty": Triggered when changing from some ranges to no ranges. No elements passed.
+*
+*/
+
+(function ($) {
+
+	"use strict";
+
+	$.fn.datesRangesSelector = function(action, options) {
+
+		var response = undefined;
+		var main_id = this.attr("id");
+		var drs_settings = $.fn.datesRangesSelector.drs_settings;
+
+		if (action == undefined || typeof action == "object") {
+
+			// Get the options if proceed
+			if (typeof action == "object") options = action;
+			var settings = $.extend({
+				new_date_range_text : "+ Add a new date range",
+				main_class_prefix : "drs",
+				date_format : "D, dd/mm/yy",
+				selector : true,
+				selector_name : "appear",
+				selector_options : [ ["Display", "1"], ["Don't display", "0"] ],
+				use_timezone_offset : true
+			}, options);
+
+			$("#" + main_id).html("<button class='drs_add_new_date_range_button' type='button' onclick='jQuery(\"#" + main_id + "\").datesRangesSelector(\"addDateRange\")' >" + settings.new_date_range_text + "</button>");
+
+			// add class
+			$("#" + main_id).addClass(settings.main_class_prefix);
+
+			// Save main settings
+			$.fn.datesRangesSelector.drs_settings = settings;
+
+			// Save enabled / disabled status
+			$.fn.datesRangesSelector.drs_enabled = true;
+
+			response = this;
+
+		} else if (action == "remove") {
+			$("#" + main_id).empty();
+
+			response = this;
+
+		} else if (action == "addDateRange") {
+
+			// options allowed: "date_begin", "date_end" (unixtime)
+			var settings = $.extend({
+				date_begin : undefined,
+				date_end : undefined,
+				selector : undefined,
+			}, options);
+
+			// hide start button just in case
+			$("#" + main_id).find(".drs_add_new_date_range_button").hide();
+
+			// remove the old "+" buttons
+			$("#" + main_id).find(".drs_add_date_range_button").hide();
+
+			// get the number of current elements
+			var current_elements = $("#" + main_id).find(".line").length;
+
+			// compose the new element
+			var new_element = '<div class="line" data-position="' + (current_elements + 1) + '"> \
+				<div class="element"> \
+					<input type="text" class="date_begin" placeholder="Begin" value="" /> \
+				</div> \
+				<div class="element"> \
+					<input type="text" class="date_end" placeholder="End" value="" /> \
+				</div>';
+
+			if (drs_settings.selector && drs_settings.selector_options.length > 0) {
+
+				var selector_options = '';
+				$.each(drs_settings.selector_options, function(index, value) {
+					if (typeof value == "object" && value.length == 2) {
+						selector_options += '<option value="' + value[1] + '">' + value[0] + '</option>';
+					}
+				});
+
+				new_element += '<div class="element"> \
+					<select> \
+						' + selector_options + ' \
+					</select> \
+				</div>';
+			}
+				
+			new_element += '<div class="element"> \
+					<button class="drs_remove_date_range_button" type="button" onclick="jQuery(\'#' + main_id + '\').datesRangesSelector(\'removeDateRange\', ' + (current_elements + 1) + ')" >-</button> \
+					<button class="drs_add_date_range_button" type="button" onclick="jQuery(\'#' + main_id + '\').datesRangesSelector(\'addDateRange\')" >+</button> \
+				</div> \
+			</div>';
+
+			// append
+			$("#" + main_id).append(new_element);
+
+			// turn the inputs into datepickers
+			var datepicker_begin = $("#" + main_id).find(".date_begin").last();
+			var datepicker_end = $("#" + main_id).find(".date_end").last();
+
+			datepicker_begin.datepicker({
+				//minDate: 0,
+				maxDate: "+1Y",
+				changeMonth: true,
+				numberOfMonths: 2,
+				dateFormat: drs_settings.date_format,
+				onClose: function( selectedDate ) {
+					datepicker_end.datepicker( "option", "minDate", selectedDate );
+					datepicker_end.datepicker("show");
+				}
+			});
+
+			datepicker_end.datepicker({
+				//minDate: 0,
+				maxDate: "+1Y",
+				changeMonth: true,
+				numberOfMonths: 2,
+				dateFormat: drs_settings.date_format,
+				onClose: function( selectedDate ) {
+					
+				}
+			});
+
+			// Set the values (if provided)
+			if (settings.date_begin != undefined) {
+				datepicker_begin.datepicker("setDate", $.datepicker.parseDate('@', parseInt(settings.date_begin) * 1000));
+			}
+
+			if (settings.date_end != undefined) {
+				datepicker_end.datepicker("setDate", $.datepicker.parseDate('@', parseInt(settings.date_end) * 1000));
+			}
+
+			if (drs_settings.selector && settings.selector != undefined) {
+				$("#" + main_id).find("select").last().val(settings.selector);
+			}
+
+			// Check if we're disabled or not
+			if (!$.fn.datesRangesSelector.drs_enabled) {
+				$("#" + main_id).find("button,input,select").attr("disabled", true);
+			}
+
+			// Triggers
+			$("#" + main_id).trigger("datesRangesSelector.rangeAdded", [current_elements + 1, settings.date_begin, settings.date_end, settings.selector]);
+			if (current_elements == 0) {
+				// we're creating "the first one"
+				$("#" + main_id).trigger("datesRangesSelector.becameFull", [current_elements + 1, settings.date_begin, settings.date_end, settings.selector]);
+			}
+
+			response = this;
+
+		} else if (action == "removeDateRange") {
+
+			// options: element to remove
+
+			if (options) {
+
+				var element = options;
+
+				// remove the element
+				$("#" + main_id).find("[data-position=" + element + "]").remove();
+
+				// show the last "+" button
+				$("#" + main_id).find(".drs_add_date_range_button").last().show();				
+
+				if ($("#" + main_id).find("[data-position]").length == 0) {
+					// show start button if no more elements are in the list
+					$("#" + main_id).find(".drs_add_new_date_range_button").show();
+					var turning_empty = true
+				} else {
+					// recalc elements positions
+					var element_position = 1;
+					$.each($("#" + main_id).find("[data-position]"), function(index, value) {
+
+						$(value).attr("data-position", element_position);
+						$(value).find(".drs_remove_date_range_button").attr("onclick", "jQuery('#" + main_id + "').datesRangesSelector('removeDateRange', " + element_position + ")");
+
+						element_position++;
+					});
+				}
+
+				// Triggers
+				$("#" + main_id).trigger("datesRangesSelector.rangeRemoved", [element]);
+				if (turning_empty) {
+					$("#" + main_id).trigger("datesRangesSelector.becameEmpty", [element]);
+				}
+			}
+
+			response = this;
+
+		} else if (action == "removeAllDateRanges") {
+
+			if ($("#" + main_id).find("[data-position]").length > 0) {
+				 // only if we have previous elements here. Otherwise it's already empty!
+				var turning_empty = true;
+			}
+
+			// remove all the elements
+			$("#" + main_id).find(".line").remove();
+			
+			// show start button
+			$("#" + main_id).find(".drs_add_new_date_range_button").show();
+
+			// Triggers
+			$("#" + main_id).trigger("datesRangesSelector.allRangesRemoved", []);
+			if (turning_empty) {
+				$("#" + main_id).trigger("datesRangesSelector.becameEmpty", []);
+			}
+
+			response = this;
+
+		} else if (action == "getDateRanges") {
+
+			response = [];
+
+			// only get if not disabled
+			if ($.fn.datesRangesSelector.drs_enabled) {
+
+				$.each($("#" + main_id).find(".line"), function(index, value) {
+
+					element = {};
+
+					var date_begin = parseInt( $.datepicker.formatDate("@", $.datepicker.parseDate(drs_settings.date_format, $(value).find(".date_begin").last().val())) );
+					var date_end = parseInt( $.datepicker.formatDate("@", $.datepicker.parseDate(drs_settings.date_format, $(value).find(".date_end").last().val())) );
+
+					if (!isNaN(date_begin) && !isNaN(date_end)) {
+						var date_begin_offset = 0;
+						var date_end_offset = 0;
+						if (drs_settings.use_timezone_offset) {
+							date_begin_offset = (new Date(date_begin).getTimezoneOffset()) * 60;
+							date_end_offset = (new Date(date_end).getTimezoneOffset()) * 60;
+						}
+
+						element.date_begin = (date_begin / 1000 - date_begin_offset);
+						element.date_end = (date_end / 1000 - date_end_offset);
+
+						if (drs_settings.selector) {
+							element[drs_settings.selector_name] = $(value).find("select").last().val();
+						}
+
+						response.push(element);
+					}
+
+				});
+
+			}
+
+		} else if (action == "disable") {
+
+			$("#" + main_id).find("button,input,select").attr("disabled", true);
+
+			// Main flag
+			$.fn.datesRangesSelector.drs_enabled = false;
+
+			response = this;
+
+		} else if (action == "enable") {
+
+			$("#" + main_id).find("button,input,select").attr("disabled", false);
+
+			// Main flag
+			$.fn.datesRangesSelector.drs_enabled = true;
+
+			response = this;
+
+		}
+
+		return response;
+
+	}
+
+
+}(jQuery));
